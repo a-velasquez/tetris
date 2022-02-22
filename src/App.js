d38145662d4b652d1aa20c3befa922d40c222156
Alex Velazquez <avelasquez075@gmail.com>
2022-02-22 14:42:00 -0800

    import cell component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Cell from "./components/Cell"
 import Tetris from "./components/Tetris"
 
 const App = () => (
 	<div className="App">
 		<Tetris />
+		<Cell />
 	</div>
 )
 
