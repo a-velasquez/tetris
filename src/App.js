ceb5b02ca6737711791e07b27048c471180b030f
Alex Velazquez <avelasquez075@gmail.com>
2022-02-25 14:16:53 -0800

    remove Cell component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react"
-import Cell from "./components/Cell"
 import Tetris from "./components/Tetris"
 
 const App = () => (
 	<div className="App">
 		<Tetris />
-		<Cell />
 	</div>
 )
 
