042c23fd7dab01d0a5f1c152a5061da0b7866e57
Alex Velazquez <avelasquez075@gmail.com>
2022-02-28 15:03:21 -0800

    map over stage prop, render cell component for every row

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Cell from "./Cell"
 
+// map over stage prop, we get a row and each row is an array that holds the cell,
+// so for every row we render a cell component
+// type = first value in the cell array
+
 const Stage = ({ stage }) => (
 	<div>
-		<Cell />
+		{stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
 	</div>
 )
 
