7274994681d1fd9485606fa5f3228e7f7064a8b9
Alex Velazquez <avelasquez075@gmail.com>
2022-03-07 12:20:09 -0800

    import usePlayer, useStage, pass stage 2 <Stage/>

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,10 @@
-import React from "react"
-import { createStage } from "../gameHelpers"
+import React, { useState } from "react"
+// style components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris"
+
+//custom hooks
+import { usePlayer } from "../hooks/usePlayer"
+import { useStage } from "../hooks/useStage"
 
 //Components
 import Stage from "./Stage"
@@ -8,10 +12,17 @@ import Display from "./Display"
 import StartButton from "./StartButton"
 
 const Tetris = () => {
+	const [dropTime, setDropTime] = useState(null)
+	const [gameOver, setGameOver] = useState(false)
+
+	const [player] = usePlayer()
+	const [stage, setStage] = useStage(player)
+
+	console.log("re-render")
 	return (
 		<StyledTetrisWrapper>
 			<StyledTetris>
-				<Stage stage={createStage()} />
+				<Stage stage={stage} />
 				<aside>
 					<div>
 						<Display text="Score" />
