9972b66e3986fae97fb0acca38e9969db678cdd5
Alex Velazquez <avelasquez075@gmail.com>
2022-03-12 22:55:59 -0800

    remove notes

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,11 +1,6 @@
 export const STAGE_WIDTH = 12
 export const STAGE_HEIGHT = 20
 
-// creating a new array using STAGE_HEIGHT
-// using an inline function to create new array
-// using STAGE_WIDTH and filling with an array of '0' or
-// empty cell, and 'clear' which has no tetromino and will be cleared in next render
-
 export const createStage = () =>
 	Array.from(Array(STAGE_HEIGHT), () => Array(STAGE_WIDTH).fill([0, "clear"]))
 
